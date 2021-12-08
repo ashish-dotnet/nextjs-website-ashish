6ffe23cfc75a9b72b93341651d7bb1754419f73f
AshishShuklaSE <75791255+ashish-dotnet@users.noreply.github.com>
2021-12-08 13:21:58 +0530

    Update event-list.js

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -3,13 +3,14 @@ import classes from './event-list.module.css'
 
 function EventList(props) {
     const {items} = props;
+    const prefix = '/nextjs-website-ashish/events/'
     return (
         <ul className={classes.list}>
             {items.map(event => <EventItem
                 key={event.id}
                 id={event.id}
                 title={event.title}
-                image={event.image}
+                image={prefix + event.image}
                 date={event.date}
                 location={event.location}
             />)}
